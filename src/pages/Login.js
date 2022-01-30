58d6cc03f2238c8404f029a944b83b7c0c54e120
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-30 17:53:40 +0700

    adding html element and some styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
 
 export default function Login() {
-  return <h2>login page</h2>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={loginImg} alt="Login logo" />
+        <button className="btn">Sign In</button>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
