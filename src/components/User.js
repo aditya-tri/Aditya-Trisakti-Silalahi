d16f5340196e9267122edf5ae33fa90c3583a9a6
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-30 22:34:04 +0700

    adding card and followers component and give some styling

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import Card from "./Card";
+import Followers from "./Followers";
 
 export default function User() {
-  return <h2>search component</h2>;
+  return (
+    <Wrapper>
+      <Card />
+      <Followers />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  /* align-items: start; */
+`;
