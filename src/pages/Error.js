0e57ae5d055bf7f73160960285f912251e70523a
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-30 17:40:28 +0700

    adding html element and styling with styled components

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Error() {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, The Page You Tried Cannot Be Found :(</h3>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
