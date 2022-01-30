0cbffb25e56266ca1b09097fea5ae71378fec450
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-30 21:18:11 +0700

    passing mosk data

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import mockRepos from "./mockData/mockRepos";
+import mockUser from "./mockData/mockUser";
+import mockFollowers from "./mockData/mockFollowers";
 
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
