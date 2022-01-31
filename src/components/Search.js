4b1894817c09515ea401fd7377ccf6c9b4224670
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-31 08:14:23 +0700

    adding local state and adding form element

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
 
 export default function Search() {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState("");
+  const handleSubmit = (e) => {
+    console.log(user);
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input type="text" placeholder="Enter Github User" />
+          </div>
+        </form>
+      </Wrapper>
+    </section>
+  );
 }
+
+const Wrapper = styled.div``;
