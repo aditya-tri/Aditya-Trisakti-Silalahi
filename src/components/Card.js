36b29838060f687b1bf3a46a1541e4e1f0c0594e
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-31 07:23:28 +0700

    increase  width and length of the user avatar

--- src/components/Card.js
+++ src/components/Card.js
@@ -79,8 +79,8 @@ const Wrapper = styled.article`
     column-gap: 1rem;
     margin-bottom: 1rem;
     img {
-      width: 75px;
-      height: 75px;
+      width: 100px;
+      height: 100px;
       border-radius: 50%;
     }
     h4 {
