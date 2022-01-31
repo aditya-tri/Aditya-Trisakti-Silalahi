33c652558176acca82faaf4c1d6b56fa3a991e55
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-31 08:44:36 +0700

    fetching github api with axios and check request limit

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import mockRepos from "./mockData/mockRepos";
 import mockUser from "./mockData/mockUser";
 import mockFollowers from "./mockData/mockFollowers";
+import axios from "axios";
+
+const githubUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
@@ -9,6 +12,21 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+
+  // Request Loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // check rate
+  const checkRequests = () => {
+    axios(`${githubUrl}/rate_limit`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(checkRequests, []);
   return (
     <GithubContext.Provider value={{ githubUser, repos, followers }}>
       {children}
