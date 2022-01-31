dcca18a802bf878339d4534f17debbffc252cebe
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-31 10:30:42 +0700

    remove followers hook state

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import mockRepos from "./mockData/mockRepos";
 import mockUser from "./mockData/mockUser";
-import mockFollowers from "./mockData/mockFollowers";
 import axios from "axios";
 
 const githubAPI = "https://api.github.com";
@@ -11,7 +10,6 @@ const GithubContext = React.createContext();
 const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
-  const [followers, setFollowers] = useState(mockFollowers);
 
   // Request Loading
   const [requests, setRequests] = useState(0);
@@ -66,7 +64,6 @@ const GithubProvider = ({ children }) => {
       value={{
         githubUser,
         repos,
-        followers,
         requests,
         error,
         searchGithubUser,
